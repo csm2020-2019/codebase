a41256d5cfe4b6bf4e43975b620df9e9ff7d2aa9
BuildTools <unconfigured@null.spigotmc.org>
2019-02-05 10:50:51 +0000

    Basic layout example for JAVA application

---
+++ src/Health_System_Monitoring/GP_GUI.java
@@ -0,0 +1,50 @@
+package Health_System_Monitoring;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class GP_GUI {
+    public static JFrame mainFrame;
+    private static JLabel headerLabel1;
+    private static JPanel controlPanel;
+
+    public static void prepareAddGPGUI() {
+
+        Main_GUI.mainFrame.setVisible(false);
+
+        mainFrame = new JFrame("GP application");
+        mainFrame.setSize(350, 190);
+        mainFrame.setLayout(new GridLayout());
+        mainFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+
+        controlPanel = new JPanel();
+        controlPanel.setLayout(new FlowLayout());
+
+        headerLabel1 = new JLabel("", JLabel.CENTER);
+        headerLabel1.setText("Application stuff");
+
+        controlPanel.add(headerLabel1);
+        BackButton();
+
+        mainFrame.add(controlPanel);
+        mainFrame.setVisible(true);
+    }
+
+    public static void BackButton() {
+        JButton BackButton = new JButton("Back");
+        BackButton.setActionCommand("Back");
+        BackButton.addActionListener(new Main_GUI.ButtonClickListener());
+        controlPanel.add(BackButton);
+    }
+
+    public static void GoBackToMainGUI() {
+        mainFrame.setVisible(false);
+        Main_GUI.mainFrame.setVisible(true);
+    }
+}
