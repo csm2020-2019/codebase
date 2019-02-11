3a10db6a91d7ca757d2650c7c869c663820bc1f1
matthew <kelechiokwuriki@gmail.com>
2019-02-11 10:56:43 +0000

    Add new patients to database method

--- src/Health_System_Monitoring/src/database_driver.java
+++ src/Health_System_Monitoring/src/database_driver.java
@@ -1,6 +1,8 @@
 //singleton class to get database connection
 
 import java.sql.*;
+import java.util.Calendar;
+
 
 /*
  * TODO:encrypt and decrypt password
@@ -15,7 +17,6 @@ import java.sql.*;
 public class database_driver {
 	private static database_driver database_driver = null;
 	private Connection databaseConnection = null; 
-	private PreparedStatement sqlStatement = null;
 	
 	//id of user from database - user table
 	private int userId = 0;
@@ -75,16 +76,19 @@ public class database_driver {
 	 */
 	public int checkCredentials(String username, String userPassword) {
 		ResultSet resultSet = null;
+		PreparedStatement sqlStatement = null;
+
 		
 		if(username != null && userPassword != null) {
-			String query = "SELECT * FROM `user` WHERE `username`=? AND `userPassword`=?";
-			
 			try {
-				sqlStatement = databaseConnection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+				String query = "SELECT * FROM `user` WHERE `username`=? AND `userPassword`=?";
+				
+				sqlStatement = databaseConnection.prepareStatement(query);
 				sqlStatement.setString(1, username);
 				sqlStatement.setString(2, userPassword);
 				
 				resultSet = sqlStatement.executeQuery();
+				closeDbConnection();
 				
 				if(resultSet.next()) {
 					userId = resultSet.getInt("userId");
@@ -106,7 +110,51 @@ public class database_driver {
 	
 	/*
 	 * - method to add new patients to database
+	 * -clarify, to add user_id?
 	 */
+	public boolean addNewPatientsToDatabase(Date patient_dob, String patient_address, String patient_medical_history,
+			String patient_diagnosis, String patient_prescriptions, int gp_id) {
+		
+		ResultSet resultSet = null;
+		PreparedStatement sqlStatement = null;
+		
+		//NULL CHECKER FOR THE  METHOD ARGUMENTS
+		if(patient_dob != null && patient_address != null & patient_medical_history != null
+				&& patient_diagnosis != null && patient_prescriptions != null && gp_id > 0) {
+			
+			//create a date object to be used for the patient dob
+			Calendar calender = Calendar.getInstance();
+			patient_dob = new Date(calender.getTime().getTime());
+			
+			try {
+				String query = "INSERT INTO patient_records (patient_dob, patient_address, patient_medical_history,"
+						+ "patient_diagnosis, patient_prescriptions, gp_id)" + " values (?, ?, ?, ?, ?, ?)";
+				
+				//create mysql prepared statement
+				sqlStatement = databaseConnection.prepareStatement(query);
+				sqlStatement.setDate(1, patient_dob);
+				sqlStatement.setString(2, patient_address);
+				sqlStatement.setString(3, patient_medical_history);
+				sqlStatement.setString(4, patient_diagnosis);
+				sqlStatement.setString(5, patient_prescriptions);
+				sqlStatement.setInt(6, gp_id);
+				
+				sqlStatement.executeUpdate();
+				closeDbConnection();
+
+				System.out.println("Patient record added");
+				
+				return true;
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+			}
+		}
+		
+		
+		return false;
+	}
 	
 
 }
