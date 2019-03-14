b10138b602f30054801bbe5807451758f344413b
matthew <kelechiokwuriki@gmail.com>
2019-03-14 18:26:22 +0000

    Implemented edit nice results method

--- src/Health_System_Monitoring/src/Health_System_Monitoring/patientDao.java
+++ src/Health_System_Monitoring/src/Health_System_Monitoring/patientDao.java
@@ -4,7 +4,9 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.math.BigDecimal;
 import java.sql.*;
 
 
@@ -252,5 +254,171 @@ public class patientDao implements patientDaoInterface{
 
         return new Patient(patient_id, patient_userid, patient_first_name, patient_last_name, patient_dob, patient_address,
                 patient_medical_history, patient_diagnosis, patient_prescriptions);
+    }
+
+    /*
+    method to add nice results to database
+    @param nice results parameters
+    @return true if successfully added to database
+     and false if not.
+     */
+    public boolean addNiceResults(int patient_id, int user_id, String sex, int age, Date result_date,
+                                  int height, int weight, int systolic_bp, int diastolic_bp, boolean smoker,
+                                  BigDecimal haemoglobin, BigDecimal urinary_albumin, int serum_creatinine, BigDecimal egfr,
+                                  BigDecimal total_cholesterol, BigDecimal ldl_level, boolean kidney_damage, boolean eye_damage,
+                                  boolean cercbrovascular_damage, boolean vision_loss, boolean eye_haemorrhage,
+                                  boolean retinal_detachment, boolean rubeosis, boolean lack_senastion, boolean deformity,
+                                  boolean foot_palpitation, boolean inappropriate_behaviour) {
+
+        if(patient_id > 0 && user_id > 0 && sex != null && age > 0 && result_date != null &&
+                height > 0 && weight > 0) {
+
+            //create a date object to be used for the result_date
+            Calendar calender = Calendar.getInstance();
+            result_date = new Date(calender.getTime().getTime());
+
+            Connection databaseConnection = database_driver.getConnection();
+
+            PreparedStatement sqlStatement = null;
+
+            try {
+                String query = "INSERT INTO nice_results (patient_id, user_id, sex, age, result_date, height, weight, " +
+                        "systolic_bp, diastolic_bp, smoker, haemoglobin, urinary_albumin, serum_creatinine, egfr, " +
+                        "total_cholesterol, ldl_level, kidney_damage, eye_damage, cerebrovascular_damage, vision_loss, " +
+                        "eye_haemorrhage, retinal_detachment, rubeosis, lack_senastion, deformity, foot_palpitation, " +
+                        "inappropriate_behaviour)" + "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, " +
+                        "?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+                sqlStatement = databaseConnection.prepareStatement(query);
+                sqlStatement.setInt(1, patient_id);
+                sqlStatement.setInt(2, user_id);
+                sqlStatement.setString(3, sex);
+                sqlStatement.setInt(4, age);
+                sqlStatement.setDate(5, result_date);
+                sqlStatement.setInt(6, height);
+                sqlStatement.setInt(7, weight);
+                sqlStatement.setInt(8, systolic_bp);
+                sqlStatement.setInt(9, diastolic_bp);
+                sqlStatement.setBoolean(10, smoker);
+                sqlStatement.setBigDecimal(11, haemoglobin);
+                sqlStatement.setBigDecimal(12, urinary_albumin);
+                sqlStatement.setInt(13, serum_creatinine);
+                sqlStatement.setBigDecimal(14, egfr);
+                sqlStatement.setBigDecimal(15, total_cholesterol);
+                sqlStatement.setBigDecimal(16, ldl_level);
+                sqlStatement.setBoolean(17, kidney_damage);
+                sqlStatement.setBoolean(18, eye_damage);
+                sqlStatement.setBoolean(19, cercbrovascular_damage);
+                sqlStatement.setBoolean(20, vision_loss);
+                sqlStatement.setBoolean(21, eye_haemorrhage);
+                sqlStatement.setBoolean(22, retinal_detachment);
+                sqlStatement.setBoolean(23, rubeosis);
+                sqlStatement.setBoolean(24, lack_senastion);
+                sqlStatement.setBoolean(25, deformity);
+                sqlStatement.setBoolean(26, foot_palpitation);
+                sqlStatement.setBoolean(27, inappropriate_behaviour);
+
+                sqlStatement.executeUpdate();
+                System.out.println("Nice result added to database");
+
+                return true;
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println(e.getMessage());
+
+            } finally {
+                if (sqlStatement != null) {
+                    try {
+                        sqlStatement.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        databaseConnection.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
+
+        return false;
+    }
+
+    public boolean updateNiceResults(int result_id, int patient_id, int user_id, String sex, int age, Date result_date,
+                                     int height, int weight, int systolic_bp, int diastolic_bp, boolean smoker,
+                                     BigDecimal haemoglobin, BigDecimal urinary_albumin, int serum_creatinine, BigDecimal egfr,
+                                     BigDecimal total_cholesterol, BigDecimal ldl_level, boolean kidney_damage, boolean eye_damage,
+                                     boolean cercbrovascular_damage, boolean vision_loss, boolean eye_haemorrhage,
+                                     boolean retinal_detachment, boolean rubeosis, boolean lack_senastion, boolean deformity,
+                                     boolean foot_palpitation, boolean inappropriate_behaviour){
+
+        if(result_id > 0 && patient_id > 0 && user_id > 0 && sex != null && age > 0 && result_date != null &&
+                height > 0 && weight > 0){
+
+            Connection databaseConnection = database_driver.getConnection();
+            PreparedStatement sqlStatement = null;
+
+            try{
+                String query = "UPDATE nice_results SET sex=?, age=?, result_date=?, height=?, weight=?, systolic_bp=?, " +
+                        "diastolic_bp=?, smoker=?, haemoglobin=?, urinary_albumin=?, serum_creatinine=?, egfr=?, " +
+                        "total_cholesterol=?, ldl_level=?, kidney_damage=?, eye_damage=?, cercbrovascular_damage=?, " +
+                        "vision_loss=?, eye_haemorrhage=?, retinal_detachment=?, rubeosis=?, lack_senastion=?, deformity=?, " +
+                        "foot_palpitation=?, inappropriate_behaviour=? where result_id=?";
+
+                sqlStatement = databaseConnection.prepareStatement(query);
+                sqlStatement.setString(1, sex);
+                sqlStatement.setInt(2, age);
+                sqlStatement.setDate(3, result_date);
+                sqlStatement.setInt(4, height);
+                sqlStatement.setInt(5, weight);
+                sqlStatement.setInt(6, systolic_bp);
+                sqlStatement.setInt(7, diastolic_bp);
+                sqlStatement.setBoolean(8, smoker);
+                sqlStatement.setBigDecimal(9, haemoglobin);
+                sqlStatement.setBigDecimal(10, urinary_albumin);
+                sqlStatement.setInt(11, serum_creatinine);
+                sqlStatement.setBigDecimal(12, egfr);
+                sqlStatement.setBigDecimal(13, total_cholesterol);
+                sqlStatement.setBigDecimal(14, ldl_level);
+                sqlStatement.setBoolean(15, kidney_damage);
+                sqlStatement.setBoolean(16, eye_damage);
+                sqlStatement.setBoolean(17, cercbrovascular_damage);
+                sqlStatement.setBoolean(18, vision_loss);
+                sqlStatement.setBoolean(19, eye_haemorrhage);
+                sqlStatement.setBoolean(20, retinal_detachment);
+                sqlStatement.setBoolean(21, rubeosis);
+                sqlStatement.setBoolean(22, lack_senastion);
+                sqlStatement.setBoolean(23, deformity);
+                sqlStatement.setBoolean(24, foot_palpitation);
+                sqlStatement.setBoolean(25, inappropriate_behaviour);
+
+                sqlStatement.executeUpdate();
+                System.out.println("Nice result updated");
+
+                return true;
+
+            } catch(SQLException e){
+                e.printStackTrace();
+                System.out.println(e.getMessage());
+            } finally {
+                if (sqlStatement != null) {
+                    try {
+                        sqlStatement.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        databaseConnection.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
