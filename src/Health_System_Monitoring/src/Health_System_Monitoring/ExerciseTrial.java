0d70489ae206a015ab44614327e496cacfdb2a77
Will Robinson [wgr] <wgr@imaps.aber.ac.uk>
2019-03-25 13:02:58 +0000

    Exercise table DAO first draft

---
+++ src/Health_System_Monitoring/src/Health_System_Monitoring/ExerciseTrial.java
@@ -0,0 +1,12 @@
+package Health_System_Monitoring;
+
+import java.math.BigDecimal;
+
+public class ExerciseTrial {
+    int trialId;
+    int regimeId;
+    String type;
+    int duration;
+    int intensity_speed;
+    BigDecimal intensity_slope;
+}
