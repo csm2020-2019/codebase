f332dc2dcb65d996cc17ebdd84a57e6ac5a35720
Nick <nicholas_masln@hotmail.co.uk>
2019-03-29 17:15:09 +0000

    Different graph depending on selection

--- src/Health_System_Monitoring/src/Health_System_Monitoring/Compare_Results.java
+++ src/Health_System_Monitoring/src/Health_System_Monitoring/Compare_Results.java
@@ -1,11 +1,14 @@
 package Health_System_Monitoring;
 
-import javax.swing.*;
+import org.jfree.ui.RefineryUtilities;
 
-public class Compare_Results extends JOptionPane {
+import javax.swing.*;
+import java.awt.*;
+
+public class Compare_Results {
 
     private static JRadioButton weightBtn, bpBtn, haemoglobinBtn, urinaryBtn, serumBtn, cholesterolBtn;
-    private static JPanel panel;
+    private static JPanel panel, north, center;
 
     public static void DisplayPanel() {
         JLabel message = new JLabel("Select which graph to display:");
@@ -18,13 +21,21 @@ public class Compare_Results extends JOptionPane {
         cholesterolBtn = new JRadioButton("Cholesterol");
 
         panel = new JPanel();
-        panel.add(message);
-        panel.add(weightBtn);
-        panel.add(bpBtn);
-        panel.add(haemoglobinBtn);
-        panel.add(urinaryBtn);
-        panel.add(serumBtn);
-        panel.add(cholesterolBtn);
+        panel.setLayout(new BorderLayout());
+
+        north = new JPanel();
+        center = new JPanel();
+
+        north.add(message);
+        center.add(weightBtn);
+        center.add(bpBtn);
+        center.add(haemoglobinBtn);
+        center.add(urinaryBtn);
+        center.add(serumBtn);
+        center.add(cholesterolBtn);
+
+        panel.add(north, BorderLayout.NORTH);
+        panel.add(center, BorderLayout.CENTER);
 
         ButtonGroup group = new ButtonGroup();
         group.add(weightBtn);
@@ -35,5 +46,43 @@ public class Compare_Results extends JOptionPane {
         group.add(cholesterolBtn);
 
         JOptionPane.showMessageDialog(null, panel);
+
+        if (weightBtn.isSelected()) {
+            System.out.println("weight");
+            result_graph graph = new result_graph("Weight Chart", "Patients Weight Over Time", "Date", "kg");
+            graph.pack();
+            RefineryUtilities.centerFrameOnScreen(graph);
+            graph.setVisible(true);
+        } else if (bpBtn.isSelected()){
+            System.out.println("blood pressure");
+            result_graph graph = new result_graph("Blood Pressure Chart", "Patients Blood Pressure Over Time", "Date", "mmHg");
+            graph.pack();
+            RefineryUtilities.centerFrameOnScreen(graph);
+            graph.setVisible(true);
+        } else if (haemoglobinBtn.isSelected()) {
+            System.out.println("haemoglobin");
+            result_graph graph = new result_graph("Haemoglobin Chart", "Patients Haemoglobin Count Over Time", "Date", "% HbA1c");
+            graph.pack();
+            RefineryUtilities.centerFrameOnScreen(graph);
+            graph.setVisible(true);
+        } else if (urinaryBtn.isSelected()) {
+            System.out.println("urinary");
+            result_graph graph = new result_graph("Urinary Albumin Chart", "Patients Urinary Albumin Over Time", "Date", "mg/mmol");
+            graph.pack();
+            RefineryUtilities.centerFrameOnScreen(graph);
+            graph.setVisible(true);
+        } else if (serumBtn.isSelected()) {
+            System.out.println("serum creatine");
+            result_graph graph = new result_graph("Serum Creatine Chart", "Patients Serum Creatine Over Time", "Date", "micromol/L");
+            graph.pack();
+            RefineryUtilities.centerFrameOnScreen(graph);
+            graph.setVisible(true);
+        } else if (cholesterolBtn.isSelected()){
+            System.out.println("cholesterol");
+            result_graph graph = new result_graph("Cholesterol Chart", "Patients Cholesterol Over Time", "Date", "Total Cholesterol");
+            graph.pack();
+            RefineryUtilities.centerFrameOnScreen(graph);
+            graph.setVisible(true);
+        }
     }
 }
